9b7fb0d032509a204c50147ab896d4dfca4c6314
sparky93 <sparkos19931003@gmail.com>
2016-12-10 23:00:47 +0200

    Refactorized cron-job

--- v1/cron-job.php
+++ v1/cron-job.php
@@ -3,21 +3,27 @@
 include 'config.inc.php';
 require_once 'Constants.class.php';
 require_once 'QueryHelper.class.php';
+require_once 'UsersTableConstants.class.php';
+require_once 'ToolsTableConstants.class.php';
 
 define (TIMESTAMP, 'TIMESTAMP');
 
-$tool1_units_key = '1' . Constants::TOOL_UNITS_KEY;
+// TODO implement for more ids
 
-$tool1_unlocked_key = '1' . Constants::TOOL_UNLOCKED_KEY;
+$id = '1';
 
-$tool1_updated_date_key = '1' . Constants::TOOL_UPDATED_DATE_KEY;
+$tool1_units_key = $id . UsersTableConstants::TABLE_UNITS_KEY;
+
+$tool1_unlocked_key = $id . UsersTableConstants::TABLE_UNLOCKED_KEY;
+
+$tool1_updated_date_key = $id . UsersTableConstants::TABLE_UPDATED_DATE_KEY;
 
 $datetime = date_create()->format('Y-m-d H:i:s');
 
 $query = QueryHelper::make_update_query(Constants::USERS_TABLE, 
-	$tool1_units_key . " = " . $tool1_units_key . " + " . Constants::TOOL_1_DU . 
+	$tool1_units_key . " = " . $tool1_units_key . " + " . ToolsTableConstants::TOOL_1_DU . 
 	" , " . $tool1_updated_date_key . " = " . "'$datetime'",
-	$tool1_unlocked_key . " = " . '1' . " AND " . 
+	$tool1_unlocked_key . " = " . $id . " AND " . 
 	$tool1_updated_date_key . " < DATE_SUB(NOW(), INTERVAL '24' HOUR) ");
 
 $query_params = array();
